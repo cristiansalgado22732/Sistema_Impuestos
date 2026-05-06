3c58c07cf2938417807b010e032bf1c988d2e685
criss <criss@Cris>
2026-05-05 19:23:49 -0500

    primer commit

---
+++ src/main/java/com/mycompany/ssistema_impuestos/Ssistema_Impuestos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ssistema_impuestos;
+
+/**
+ *
+ * @author criss
+ */
+public class Ssistema_Impuestos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

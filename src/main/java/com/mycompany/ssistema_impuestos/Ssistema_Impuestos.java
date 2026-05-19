467add63bf97c02cf8392f6f53df81cab81f69e6
criss <criss@Cris>
2026-05-18 20:16:11 -0500

    Se reinicio de proyecto

--- src/main/java/com/mycompany/ssistema_impuestos/Ssistema_Impuestos.java
+++ src/main/java/com/mycompany/ssistema_impuestos/Ssistema_Impuestos.java
@@ -11,6 +11,6 @@ package com.mycompany.ssistema_impuestos;
 public class Ssistema_Impuestos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
